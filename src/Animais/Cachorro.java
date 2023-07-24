0efc869ff07a764477d1474265caca281018b752
WMatheus2022 <pachwander4@gmail.com>
2023-07-24 19:21:10 -0300

    modified:   bin/Animais/Cachorro.class 	modified:   bin/Main.class 	modified:   src/Animais/Cachorro.java 	modified:   src/Main.java

--- src/Animais/Cachorro.java
+++ src/Animais/Cachorro.java
@@ -2,61 +2,197 @@ package Animais;
 
 public class Cachorro {
 
-    public String nome;
-    public String Raca;
-    public String Cor;
-    public int Altura;
-    public double peso;
-    public int tamanhoRabo;
+    private String nome;
+    private String Raca;
+    private String Cor;
+    private int Altura;
+    private double peso;
+    private int tamanhoRabo;
+
+    //cachorro1
+    public Cachorro(){}; // constrtur padrao
+
+    //cachorro2
+    public Cachorro(String nome, String raca, String cor, int altura, double peso, int tamanhoRabo,
+            String estadoEspirito) {
+        this.nome = nome;
+        Raca = raca;
+        Cor = cor;
+        Altura = altura;
+        this.peso = peso;
+        this.tamanhoRabo = tamanhoRabo;
+        this.estadoEspirito = estadoEspirito;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRaca() {
+        return Raca;
+    }
+
+    public void setRaca(String raca) {
+        Raca = raca;
+    }
+
+    public String getCor() {
+        return Cor;
+    }
+
+    public void setCor(String cor) {
+        Cor = cor;
+    }
+
+    public int getAltura() {
+        return Altura;
+    }
+
+    public void setAltura(int altura) {
+        Altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public int getTamanhoRabo() {
+        return tamanhoRabo;
+    }
+
+    public void setTamanhoRabo(int tamanhoRabo) {
+        this.tamanhoRabo = tamanhoRabo;
+    }
+
     public String estadoEspirito;
     public String Comer;
     public String Latir;
+    public String Pegar;
 
+    public String Comer(String fome) {
 
-    public String Comer(String fome){
-        if(fome.equals("esta com fome")){
-            this.Latir = "comer comida";
-        } else{
-            this.Latir = "pedir carinho";
+        switch (fome) {
+            case "esta com fome":
+                this.Comer = "comer comida";
+                break;
+            default:
+                this.Comer = "pedir carinho";
+                break;
         }
-        return Comer;
+
+        /*
+         * if(fome.equals("esta com fome")){
+         * this.Comer = "comer comida";
+         * } else{
+         * this.Comer = "pedir carinho";
+         * }
+         */
+        return this.Comer;
     };
 
     public String Latir(String lata) {
-       if(lata.equals("ouvir um barulho estranho")){
-            this.Latir = "AU AU AU...";
-       }else if(lata.equals("Ouvir outro cachorro latindo")){
-            this.Latir = "Uivar!";
-       }else if(lata.equals("se ver ou animal")){
-            this.Latir = "latir bastante";
-       }
-       return Latir;
+
+        switch (lata) {
+            case "ouvir um barulho estranho":
+                this.Latir = "AU AU AU...";
+                break;
+            case "Ouvir outro cachorro latindo":
+                this.Latir = "Uivar!";
+                break;
+            case "se ver ou animal":
+                this.Latir = "latir bastante";
+                break;
+            default:
+                this.Latir = "ficar quieto";
+                break;
+
+        }
+
+        /*
+         * if(lata.equals("ouvir um barulho estranho")){
+         * this.Latir = "AU AU AU...";
+         * }else if(lata.equals("Ouvir outro cachorro latindo")){
+         * this.Latir = "Uivar!";
+         * }else if(lata.equals("se ver ou animal")){
+         * this.Latir = "latir bastante";
+         * }else{
+         * this.Latir = "ficar quieto";
+         * }
+         */
+        return this.Latir;
     }
 
-    public String pegar() {
-        return "Bolinha";
+    public String Pegar(String pega) {
+        switch (pega) {
+            case "Jogar Bolinha":
+                this.Pegar = "Pegar bolinha";
+                break;
+            case "Jogar osso":
+                this.Pegar = "Pegar osso";
+                break;
+            case "Jogar graveto":
+                this.Pegar = "Pegar graveto";
+                break;
+            default:
+                this.Pegar = "Pedir para brincar";
+                break;
+
+        }
+        return this.Pegar;
     }
 
     public String interagir(String acao) {
-        if (acao.equals("carinho")) {
-            this.estadoEspirito = "feliz";
 
-        } else if(acao.equals("Levar para pessear")){
-            this.estadoEspirito = "Alegre";
-            
-        } else if(acao.equals("pisar na patinha")){
-            this.estadoEspirito = "Triste";
-
-        } else if(acao.equals("pisar no rabo")){
-            this.estadoEspirito = "Bravo";
-
-        } else if (acao.equals("vai dormir!")) {
-            this.estadoEspirito = "chateado";
-
-        } else {
-            this.estadoEspirito = "neutro";
-
+        switch (acao) {
+            case "carinho":
+                this.estadoEspirito = "feliz";
+                break;
+            case "Levar para pessear":
+                this.estadoEspirito = "Alegre";
+                break;
+            case "pisar na patinha":
+                this.estadoEspirito = "Triste";
+                break;
+            case "pisar no rabo":
+                this.estadoEspirito = "Bravo";
+                break;
+            case "vai dormir!":
+                this.estadoEspirito = "chateado";
+                break;
+            default:
+                this.estadoEspirito = "neutro";
+                break;
         }
-        return estadoEspirito;
+
+        /*
+         * if (acao.equals("carinho")) {
+         * this.estadoEspirito = "feliz";
+         * 
+         * } else if(acao.equals("Levar para pessear")){
+         * this.estadoEspirito = "Alegre";
+         * 
+         * } else if(acao.equals("pisar na patinha")){
+         * this.estadoEspirito = "Triste";
+         * 
+         * } else if(acao.equals("pisar no rabo")){
+         * this.estadoEspirito = "Bravo";
+         * 
+         * } else if (acao.equals("vai dormir!")) {
+         * this.estadoEspirito = "chateado";
+         * 
+         * } else {
+         * this.estadoEspirito = "neutro";
+         * 
+         * }
+         */
+        return this.estadoEspirito;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,26 @@ import Animais.Cachorro;
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        Cachorro cachorro1 = new Cachorro();
+        /*Cachorro cachorro1 = new Cachorro();
+        cachorro1.setNome("Luky");
+        cachorro1.setRaca("Pitbull");
+        cachorro1.setCor("Caramelo");
+        cachorro1.setAltura(60);
+        cachorro1.setPeso(5.5);
+        cachorro1.setTamanhoRabo(3);*/
 
-        cachorro1.nome = "Luky";
-        cachorro1.Raca = "Pitbull";
-        cachorro1.Cor = "Caramelo";
-        cachorro1.Altura = 60;
-        cachorro1.peso = 5.5;
-        cachorro1.tamanhoRabo = 3;
+        Cachorro cachorro2 = new Cachorro("Pateta", "labrador", "branco", 70, 15.5, 30, "alegre");
 
-        System.out.println("ouviu "+ cachorro1.Latir("ouvir um barulho estranho"));
-        System.out.println("O cachorro pegou uma " + cachorro1.pegar());
+        System.out.println(cachorro2.getNome());
+        System.out.println(cachorro2.getRaca());
+        System.out.println(cachorro2.getCor());
 
-        System.out.println("O cachorro esta " + cachorro1.interagir("carinho"));
-        System.out.println("O cachorro esta " + cachorro1.interagir("nada"));
-        System.out.println("O cachorro esta " + cachorro1.interagir("vai dormir!"));
+        System.out.println("ouviu " + cachorro2.Latir("ouvir um barulho estranho"));
+        System.out.println("O cachorro pegou uma " + cachorro2.Pegar("Jogar graveto"));
+
+        System.out.println("O cachorro esta " + cachorro2.interagir("pisar na patinha"));
+        System.out.println("O cachorro esta " + cachorro2.interagir("nada"));
+        System.out.println("O cachorro esta " + cachorro2.interagir("vai dormir!"));
 
     }
 }
